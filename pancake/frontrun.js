d723a259d6ea598b48fa7fd7233594bc5e503845
GTXT88 <92250756+GTXT88@users.noreply.github.com>
2022-02-21 18:51:55 +1300

    Add files via upload

---
+++ pancake/frontrun.js
@@ -0,0 +1,350 @@
+/**
+ * Perform a front-running attack on pancakeSwap
+*/
+//const fs = require('fs');
+var Web3 = require('web3');
+var abiDecoder = require('abi-decoder');
+var colors = require("colors");
+var Tx = require('ethereumjs-tx').Transaction;
+var axios = require('axios');
+var BigNumber = require('big-number');
+
+const {NETWORK, PANCAKE_ROUTER_ADDRESS, PANCAKE_FACTORY_ADDRESS, PANCAKE_ROUTER_ABI, PANCAKE_FACTORY_ABI, PANCAKE_POOL_ABI, HTTP_PROVIDER_LINK, WEBSOCKET_PROVIDER_LINK, HTTP_PROVIDER_LINK_TEST} = require('./constants.js');
+const {setBotAddress, getBotAddress, FRONT_BOT_ADDRESS, botABI} = require('./bot.js');
+const {PRIVATE_KEY, TOKEN_ADDRESS, AMOUNT, LEVEL} = require('./env.js');
+
+const INPUT_TOKEN_ADDRESS = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
+const WBNB_TOKEN_ADDRESS = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
+
+var input_token_info;
+var out_token_info;
+var pool_info;
+var gas_price_info;
+
+var web3;
+var web3Ts;
+var web3Ws;
+var pancakeRouter;
+var pancakeFactory;
+
+// one gwei
+const ONE_GWEI = 1e9;
+
+var buyer_input_amount = 10 * (10**18); //10WBNB
+
+async function createWeb3(){
+    try {
+        web3 = new Web3(new Web3.providers.HttpProvider(HTTP_PROVIDER_LINK));
+        web3Ts = new Web3(new Web3.providers.HttpProvider(HTTP_PROVIDER_LINK_TEST));
+        web3Ws = new Web3(new Web3.providers.WebsocketProvider(WEBSOCKET_PROVIDER_LINK));
+        pancakeRouter = new web3.eth.Contract(PANCAKE_ROUTER_ABI, PANCAKE_ROUTER_ADDRESS);
+        pancakeFactory = new web3.eth.Contract(PANCAKE_FACTORY_ABI, PANCAKE_FACTORY_ADDRESS);
+        abiDecoder.addABI(PANCAKE_ROUTER_ABI);
+
+        return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+}
+
+async function main() {
+
+    if (await createWeb3() == false) {
+        console.log('Web3 Create Error'.yellow);
+        process.exit();
+    }
+    
+    const out_token_address = TOKEN_ADDRESS;
+    const amount = AMOUNT;
+    const level = LEVEL;
+    
+    ret = await preparedAttack(INPUT_TOKEN_ADDRESS, out_token_address, amount, level); 
+    if(ret == false) {
+        process.exit();
+    }
+
+    await updatePoolInfo();
+    
+    // this calculation is based on buyer use "swapExactETHForTokens" method
+    log_str = '***** assume I frontrun ' + amount + "WBNB" + '*****' + "buyer purchase " + buyer_input_amount/(10**18) + "WBNB";
+    console.log(log_str.green);    
+    // console.log(web3Ws);
+    web3Ws.onopen = function(evt) {
+        // web3Ws.send(JSON.stringify({ method: "subscribe", topic: "transfers", address: user_wallet.address }));
+        console.log('connected')
+    }
+
+    let amountBigNumber = amount*(10**18);
+    let meFrontrunOutputAmount = await pancakeRouter.methods.getAmountOut(amountBigNumber.toString(), pool_info.input_volumn.toString(), pool_info.output_volumn.toString()).call();
+    console.log("meFrontrunOutputAmount:" + meFrontrunOutputAmount);
+    // new pool reserves after I frontrun 
+    let poolNewInputReserve = findSum(pool_info.input_volumn.toString(), amountBigNumber.toString()); 
+    let poolNewOutputReserve = longDivision(pool_info.klast.toString(), poolNewInputReserve.toString());
+    console.log(poolNewInputReserve + "." + pool_info.klast);
+
+    // new pool reserves after buyer made the purchase of WBNB
+    let newInputReserve = findSum(poolNewInputReserve.toString(), buyer_input_amount.toString());
+    let newOutputReserve = longDivision(pool_info.klast.toString(), newInputReserve.toString());
+    console.log(newInputReserve + "." + newOutputReserve);
+
+    // me backrun after buyer complete the purchase of WBNB
+    let meBackrunOutputAmount = await pancakeRouter.methods.getAmountOut(meFrontrunOutputAmount.toString(), newOutputReserve.toString(), newInputReserve.toString()).call();
+    console.log(meBackrunOutputAmount);
+    // calculate gas costs of the transactions
+    let TxGasPrice = 50*ONE_GWEI;
+    let TxGasCost = TxGasPrice * 500000 * 2;
+    console.log(TxGasCost);
+
+    // calculate profits
+    let profitCalc = parseInt(meBackrunOutputAmount) - parseInt(amountBigNumber) - parseInt(TxGasCost);
+    console.log(profitCalc);
+    let profit_est = profitCalc / (10**18);
+    console.log(profit_est);
+    process.exit();
+}
+
+function findDiff(str1,str2)
+{
+    let str = "";
+    let n1 = str1.length, n2 = str2.length;
+    str1 = str1.split("").reverse().join("")
+    str2 = str2.split("").reverse().join("")
+    let carry = 0;
+    for (let i = 0; i < n2; i++)
+    {
+        let sub
+            = ((str1[i].charCodeAt(0) -
+            '0'.charCodeAt(0))
+               - (str2[i].charCodeAt(0) -
+               '0'.charCodeAt(0)) - carry);
+
+        if (sub < 0) {
+            sub = sub + 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+
+        str += String.fromCharCode(sub +
+        '0'.charCodeAt(0));
+    }
+
+    for (let i = n2; i < n1; i++) {
+        let sub = ((str1[i].charCodeAt(0) -
+        '0'.charCodeAt(0)) - carry);
+
+        if (sub < 0) {
+            sub = sub + 10;
+            carry = 1;
+        }
+        else
+            carry = 0;
+
+        str += String.fromCharCode(sub +
+        '0'.charCodeAt(0));
+    }
+    return str.split("").reverse().join("")
+}
+
+function findSum(first, second) {
+	var sum = '';
+	var carry = 0;
+	var diff = second.length - first.length;
+	for (i = first.length - 1; i >= 0; i--) {
+		var temp =
+			(Number(first.charAt(i)) % 10) +
+			(Number(second.charAt(i + diff)) % 10) +
+			carry;
+		if (temp >= 10) {
+			sum = (temp % 10) + sum;
+			carry = Math.floor(temp / 10);
+		} else {
+			sum = temp + sum;
+			carry = 0;
+		}
+	}
+	if (carry) {
+		sum = carry + sum;
+	}
+	return sum;
+}
+
+function longDivision(number,divisor)
+{
+    let ans="";
+    let idx = 0;
+      let temp=number[idx]-'0';
+    while (temp < divisor)
+    {
+        temp = (temp * 10 +
+        (number[idx + 1]).charCodeAt(0) -
+               ('0').charCodeAt(0));
+        idx += 1;
+    }
+    idx += 1;
+     
+    while(number.length>idx)
+    {
+        // Store result in answer i.e. temp / divisor
+        ans += String.fromCharCode
+        (Math.floor(temp / divisor) +
+        ('0').charCodeAt(0));
+       
+        // Take next digit of number
+        temp = ((temp % divisor) * 10 +
+        (number[idx]).charCodeAt(0) -
+              ('0').charCodeAt(0));
+        idx += 1;
+    }
+     
+    ans += String.fromCharCode
+    (Math.floor(temp / divisor) +
+    ('0').charCodeAt(0));
+     
+    //If divisor is greater than number
+    if(ans.length==0)
+        return "0";
+    //else return ans
+    return ans;
+}
+
+async function getCurrentGasPrices() {
+    return {low: 20, medium: 20, high:50};
+}
+
+async function updatePoolInfo() {
+    try{
+
+        var reserves = await pool_info.contract.methods.getReserves().call();
+        var kLast = await pool_info.contract.methods.kLast().call(); 
+
+        if(pool_info.forward) {
+            var eth_balance = reserves[0];
+            var token_balance = reserves[1];
+        } else {
+            var eth_balance = reserves[1];
+            var token_balance = reserves[0];
+        }
+
+        pool_info.input_volumn = eth_balance;
+        pool_info.output_volumn = token_balance;
+        pool_info.attack_volumn = eth_balance * (pool_info.attack_level/100); 
+        pool_info.klast = kLast;
+
+    }catch (error) {
+      
+        console.log('Failed To Get Pair Info'.yellow);
+
+        return false;
+    }
+}
+
+async function getPoolInfo(input_token_address, out_token_address){
+
+    var log_str = '*****\t' + input_token_info.symbol + '-' + out_token_info.symbol + ' Pair Pool Info\t*****'
+    console.log(log_str.green);
+
+    try{
+        var pool_address = await pancakeFactory.methods.getPair(input_token_address, out_token_address).call();
+        if(pool_address == '0x0000000000000000000000000000000000000000')
+        {
+            log_str = 'PanCake has no ' + out_token_info.symbol + '-' + input_token_info.symbol + ' pair';
+            console.log(log_str.yellow);
+            return false;
+        }   
+
+        var log_str = 'Address:\t' + pool_address;
+        console.log(log_str.white);
+
+        var pool_contract = new web3.eth.Contract(PANCAKE_POOL_ABI, pool_address);
+        var reserves = await pool_contract.methods.getReserves().call();
+
+        var token0_address = await pool_contract.methods.token0().call();
+
+        var kLast = await pool_contract.methods.kLast().call();
+
+        if(token0_address == INPUT_TOKEN_ADDRESS) {
+            var forward = true;
+            var bnb_balance = reserves[0];
+            var token_balance = reserves[1];
+        } else {
+            var forward = false;
+            var bnb_balance = reserves[1];
+            var token_balance = reserves[0];
+        }
+
+        var log_str = (bnb_balance/(10**input_token_info.decimals)).toFixed(5) + '\t' + input_token_info.symbol;
+        console.log(log_str.white);
+
+        var log_str = (token_balance/(10**out_token_info.decimals)).toFixed(5) + '\t' + out_token_info.symbol;
+        console.log(log_str.white);
+
+        pool_info = {'klast': kLast, 'contract': pool_contract, 'forward': forward, 'input_volumn': bnb_balance, 'output_volumn': token_balance}
+        
+        return true;
+
+    }catch(error){
+        console.log('Error: Get Pari Info')
+        return false;
+    }
+}
+
+async function getBNBInfo(user_wallet){
+    // var balance = await web3.eth.getBalance(user_wallet.address);
+    var decimals = 18;
+    var symbol = 'BNB';
+
+    return {'address': WBNB_TOKEN_ADDRESS, 'symbol': symbol, 'decimals': decimals, 'abi': null, 'token_contract': null}
+}
+
+async function getTokenInfo(tokenAddr, token_abi_ask) {
+    try{
+        //get token abi
+        var response = await axios.get(token_abi_ask);
+        if(response.data.status==0)
+        {
+            console.log('Invalid Token Address !')   
+            return null;
+        }   
+        console.log("token got");
+        var token_abi = response.data.result;
+
+        //get token info
+        var token_contract = new web3.eth.Contract(JSON.parse(token_abi), tokenAddr);
+        // console.log(token_contract);
+        
+        // var balance = await token_contract.methods.balanceOf(user_wallet.address).call();
+        var decimals = await token_contract.methods.decimals().call();
+        var symbol =  await token_contract.methods.symbol().call();
+
+        return {'address': tokenAddr, 'symbol': symbol, 'decimals': decimals, 'abi': token_abi, 'token_contract': token_contract}
+    }catch(error){
+        console.log('Unable to read Token Info, may due to a Proxy Smart contract');
+        return false;
+    }
+}
+
+async function preparedAttack(input_token_address, out_token_address, amount, level)
+{
+    input_token_info = await getBNBInfo();
+    //out token balance 
+    const OUT_TOKEN_ABI_REQ = 'https://api.bscscan.com/api?module=contract&action=getabi&address='+out_token_address+'&apikey=XIBZM1NWU7ABRB142CJJD6A8NIT8EQBYPB';
+    out_token_info = await getTokenInfo(out_token_address, OUT_TOKEN_ABI_REQ);
+    if (out_token_info == null){
+        return false;
+    }
+    
+    //check pool info
+    if(await getPoolInfo(WBNB_TOKEN_ADDRESS, out_token_address) == false)
+        return false;
+    
+    gas_price_info = await getCurrentGasPrices();
+
+    log_str = '=================== Prepared to attack '+ input_token_info.symbol + '-'+ out_token_info.symbol +' pair ==================='
+    console.log(log_str.red);
+
+    return true;
+}
+
+main();
